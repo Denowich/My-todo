f46e09674f604910adf128180d224333f01f314b
Denowich <denowich@gmail.com>
2024-05-17 15:45:39 +0300

    Fix EditableSpan component

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 
 type EditableSpanPropsType = {
       title: string;
 }
 export function EditableSpan (props: EditableSpanPropsType) {
-      return <span>{props.title}___</span>
+      let [editMode, setEditMode] = useState(false);
+      let [title, setTitle] = useState('');
+
+      const activateEditeMode = () => {
+            setEditMode(true);
+            setTitle(props.title);
+      }
+      const activateViewMode = () => {
+            setEditMode(false);
+      }
+
+
+      const onChangeTitleHandler = (e: ChangeEvent<HTMLInputElement>) => {
+            setTitle(e.currentTarget.value);
+      }
+
+      return editMode 
+      ? <input value={title} onBlur={activateViewMode} autoFocus onChange={onChangeTitleHandler} /> 
+      : <span onClick={activateEditeMode}>{props.title}___</span>
 }
